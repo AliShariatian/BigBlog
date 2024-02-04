5cb9d8df97f376d6567476c501e33fde1109cfa5
Ali Shariatian <shariatian92@gmail.com>
2024-02-04 21:23:41 +0330

    feat(app): add sign-in and sign-up page into view

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,16 +3,22 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // components import
 import Navbar from "./components/Navbar";
 
+// pages import
+import UserAuthForm from "./pages/userAuthForm";
+
 const App = () => {
    return (
       <Router>
+         {/* header */}
          <Navbar />
 
          <Routes>
             <Route path="/" element={<h1>Home</h1>} />
-            <Route path="/signin" element={<h1>in</h1>} />
-            <Route path="/signup" element={<h1>up</h1>} />
+            <Route path="/signin" element={<UserAuthForm type="sign-in" />} />
+            <Route path="/signup" element={<UserAuthForm type="sign-up" />} />
          </Routes>
+
+         {/* footer */}
       </Router>
    );
 };
